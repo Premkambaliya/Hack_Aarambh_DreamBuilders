f23768981d7b962504fd7cdbb4aac8e51f07c35a
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-07-10 11:37:54 +0530

    Define audio data schema

---
+++ Backend/modules/audio/audio.model.js
@@ -0,0 +1,54 @@
+// MongoDB Call Model
+// Using MongoDB native driver - no Mongoose
+
+import { getDatabase } from "../../config/db.js";
+
+class CallModel {
+  constructor() {
+    this.collectionName = "calls";
+  }
+
+  getCollection() {
+    const db = getDatabase();
+    return db.collection(this.collectionName);
+  }
+
+  async findById(callId) {
+    const collection = this.getCollection();
+    return await collection.findOne({ callId });
+  }
+
+  async findAll(query = {}) {
+    const collection = this.getCollection();
+    return await collection.find(query).toArray();
+  }
+
+  async create(callData) {
+    const collection = this.getCollection();
+    const result = await collection.insertOne({
+      ...callData,
+      createdAt: new Date(),
+    });
+    return result;
+  }
+
+  async updateOne(callId, updateData) {
+    const collection = this.getCollection();
+    return await collection.updateOne(
+      { callId },
+      { $set: { ...updateData, updatedAt: new Date() } }
+    );
+  }
+
+  async deleteOne(callId) {
+    const collection = this.getCollection();
+    return await collection.deleteOne({ callId });
+  }
+
+  async deleteByUserId(userId) {
+    const collection = this.getCollection();
+    return await collection.deleteMany({ userId: userId.toString() });
+  }
+}
+
+export default new CallModel();
